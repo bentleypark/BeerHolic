284da668815957aec468afed0ec6f34c1ecaeff3
Bentley Park <junggil.park@sovoro.kr>
2020-12-19 11:24:35 +0900

    add buildSrc to manage dependencies

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,66 @@
+object Versions {
+
+    // Class Path for Plugin
+    const val buildGradleVersion = "4.1.1"
+    const val googleServiceVersion = "4.3.3"
+    const val r8Version = "2.1.66"
+    const val detektGradleVersion = "1.12.0"
+    const val navSafArgPluginVersion = "2.3.0"
+
+    // Kotlin
+    const val kotlinVersion = "1.4.21"
+
+    // Coroutines
+    const val coroutineVersion = "1.4.2"
+
+    // RxJava
+    const val rxJavaVersion = "2.2.20"
+    const val rxAndroidVersion = "2.1.1"
+
+    const val junitVersion = "4.13.1"
+
+    // AndroidX
+    const val appCompatVersion = "1.2.0"
+    const val constraintLayoutVersion = "2.0.4"
+    const val coreKtxVersion = "1.3.2"
+    const val legacySupportVersion = "1.0.0"
+    const val activityVersion = "1.1.0"
+    const val fragmentVersion = "1.2.5"
+    const val recyclerviewVersion = "1.1.0"
+    const val databindingVersion = "4.0.1"
+
+    // Jetpack
+    const val lifeCycleVersion = "2.2.0"
+    const val navigationVersion = "2.3.2"
+    const val startupVersion = "1.0.0"
+    const val pagingVersion = "3.0.0-alpha09"
+
+    const val testVersion = "1.3.0"
+    const val espressoVersion = "3.3.0"
+    const val junitExtVersion = "1.1.2"
+
+    // Google
+    const val materialVersion = "1.2.1"
+    const val gsonVersion = "2.8.6"
+
+
+    // Square
+    const val retrofitVersion = "2.9.0"
+    const val okhttp3Version = "4.9.0"
+
+    // RxBinding
+    const val rxbindingVersion = "3.1.0"
+
+    // Debug
+    const val timberVersion = "4.7.1"
+
+    // Klint
+    const val klintVersion = "0.39.0"
+
+    // Hilt
+    const val hiltVersion = "2.30.1-alpha"
+    const val hiltViewModel = "1.0.0-alpha02"
+
+    // Coil
+    const val coilVersion = "1.1.0"
+}
